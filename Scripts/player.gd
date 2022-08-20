ad7207e991f11ae53e416eb760b8ae4e9b2c9bd5
Gustavo Henrique <ghco@cesar.school>
2022-08-20 19:22:21 -0300

    animacao correr

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -10,9 +10,10 @@ var limit_l = 11
 
 func _ready():
 	pass 
+	
+var is_runnig = false
 
 func _process(delta):
-	
 	var dirX = 0
 	
 	if Input.is_action_pressed("ui_right"):
@@ -32,9 +33,13 @@ func _process(delta):
 		
 	if global_position.x > limit_r:
 		global_position.x = limit_r
+		
 	
 	if dirX != 0:
-		sprite.animation = "walk"
+		if dirX > 1 or dirX < -1:
+			sprite.animation = "run"
+		else:
+			sprite.animation = "walk"
 	else:
 		sprite.animation = "idle"
 		
@@ -42,15 +47,3 @@ func _process(delta):
 		sprite.flip_h = true
 	elif dirX > 0:
 		sprite.flip_h = false
-
-#	$brilhinhoSprite.visible = active
-
-#func _input(event):
-	#if get_node_or_null('DialogNode') == null:
-		#if event.is_action_pressed("ui_accept") and active:
-			#get_tree().paused = true
-			#var dialog = Dialogic.start('timeline-1')
-			#dialog.pause_mode = Node.PAUSE_MODE_PROCESS
-			#dialog.connect('timeline_end', self, 'unpause')
-			#add_child(dialog)
-			
