2843905562c8108b6e0b138e87a54ba92608aa4a
Eduardo Gerab <eduardogerab@gmail.com>
2022-08-20 19:21:25 -0300

    tela cheia e diálogo

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -43,14 +43,4 @@ func _process(delta):
 	elif dirX > 0:
 		sprite.flip_h = false
 
-#	$brilhinhoSprite.visible = active
 
-#func _input(event):
-	#if get_node_or_null('DialogNode') == null:
-		#if event.is_action_pressed("ui_accept") and active:
-			#get_tree().paused = true
-			#var dialog = Dialogic.start('timeline-1')
-			#dialog.pause_mode = Node.PAUSE_MODE_PROCESS
-			#dialog.connect('timeline_end', self, 'unpause')
-			#add_child(dialog)
-			
